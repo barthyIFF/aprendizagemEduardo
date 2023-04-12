d801328b1c41bbaaf2ce782629664ddfc4ee4b12
Claudinho <barthy@gmail.com>
2023-04-12 18:11:47 -0300

    Criando um jeito de mudar o Nome do Titular da Conta

--- src/Conta.java
+++ src/Conta.java
@@ -1,13 +1,27 @@
 
 public class Conta {
-	
-	String nomeDoTitular;
-	int numeroDaConta;
-	String agencia;
-	double saldo;
-	Data dataDeAbertura;
+	/*
+	 * Eduardo, veja que o nome do 
+	 */
 	
 	
+	
+	//Veja que conforme conversamos esses são os atributos, ou seja, o que essa classe TEM
+	private String nomeDoTitular;
+	private int numeroDaConta;
+	private String agencia;
+	private double saldo;
+	private Data dataDeAbertura;
+	
+	
+	public String getNomeDoTitular() {
+		return nomeDoTitular;
+	}
+
+	public void setNomeDoTitular(String nomeDoTitular) {
+		this.nomeDoTitular = nomeDoTitular;
+	}
+
 	void saca(double valorQueSeraSacado) {
 		saldo += -valorQueSeraSacado;
 	}
@@ -20,11 +34,12 @@ public class Conta {
 		return saldo * 0.1;
 	}
 	
+	//Veja que conforme conversamos esse é um método, ou seja, o que essa classe FAZ 
 	void mostrarDadosDaConta() {
 		System.out.println("Nome do titular: " + nomeDoTitular);
 		System.out.println("Número da conta: "+ numeroDaConta);
 		System.out.println("Agência: " + agencia);
 		System.out.println("Saldo da conta: " + saldo);
-		System.out.println("Data de abertura: " + dataDeAbertura.dia + "/" + dataDeAbertura.mes + "/" + dataDeAbertura.ano );
+		//System.out.println("Data de abertura: " + dataDeAbertura.dia + "/" + dataDeAbertura.mes + "/" + dataDeAbertura.ano );
 	}
 }
